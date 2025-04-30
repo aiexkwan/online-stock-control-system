9347dc93d5652c4c320496789ae81a8cae7de207
Alex Kwan <aiexkwan@gmail.com>
2025-04-30 22:26:07 +0100

    chore: add git push script for convenience

---
+++ push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 檢查是否提供了提交訊息
+if [ -z "$1" ]; then
+  echo "請提供提交訊息，例如: ./push.sh \"修復登入頁面問題\""
+  exit 1
+fi
+
+# 添加所有更改
+git add .
+
+# 提交更改
+git commit -m "$1"
+
+# 推送到 GitHub
+git push origin main
+
+echo "✅ 已成功推送更改到 GitHub!" 
