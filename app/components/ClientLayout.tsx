f9b40d9dcc19f461aa58a28c5f228c4dafef2584
Alex Kwan <aiexkwan@gmail.com>
2025-05-01 21:47:58 +0100

    style: 採用現代深色dashboard主題與卡片化佈局，保留原有功能

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -10,21 +10,23 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
   if (hideSidebar) {
     return (
-      <div className="min-h-screen bg-gray-900 flex flex-col">
+      <div className="min-h-screen bg-[#181c2f] flex flex-col">
         {children}
       </div>
     );
   }
 
   return (
-    <div className="min-h-screen flex bg-gray-100">
-      {/* 側邊選單 2/10 */}
-      <aside className="w-1/5 min-w-[180px] max-w-xs bg-gray-200 border-r border-gray-300">
+    <div className="min-h-screen flex bg-[#181c2f]">
+      {/* 側邊選單 2/10，深灰底 */}
+      <aside className="w-1/5 min-w-[180px] max-w-xs bg-[#23263a] border-r border-[#23263a] shadow-lg flex flex-col">
         <Navigation />
       </aside>
-      {/* 主內容 8/10 */}
-      <main className="flex-1 px-6 py-8">
-        {children}
+      {/* 主內容 8/10，卡片化分區 */}
+      <main className="flex-1 px-8 py-10 overflow-y-auto">
+        <div className="max-w-7xl mx-auto space-y-8">
+          {children}
+        </div>
       </main>
     </div>
   );
