e5bf28be6858f517658d6076afe5dccfac79c646
Alex Kwan <aiexkwan@gmail.com>
2025-05-01 21:40:11 +0100

    style: 側邊選單改為淺色並調整為2:8佈局，主內容緊貼側邊欄

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -1,32 +1,19 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navigation from './Navigation';
-import { usePathname } from 'next/navigation';
-
-function SafeHydration({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-    return () => setMounted(false);
-  }, []);
-
-  return mounted ? <>{children}</> : null;
-}
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isAuthPage = pathname === '/login' || pathname === '/change-password';
-
   return (
-    <SafeHydration>
-      <div className="min-h-screen bg-gray-900">
-        {!isAuthPage && <Navigation />}
-        <div className={!isAuthPage ? 'ml-64 pt-16' : ''}>
-          {children}
-        </div>
-      </div>
-    </SafeHydration>
+    <div className="min-h-screen flex bg-gray-100">
+      {/* 側邊選單 2/10 */}
+      <aside className="w-1/5 min-w-[180px] max-w-xs bg-gray-200 border-r border-gray-300">
+        <Navigation />
+      </aside>
+      {/* 主內容 8/10 */}
+      <main className="flex-1 px-6 py-8">
+        {children}
+      </main>
+    </div>
   );
 } 
