329ac889a1eea114115207d44dc03243c4fa05ee
Alex Kwan <aiexkwan@gmail.com>
2025-05-02 23:00:41 +0100

    feat: QC Label tab click will navigate to /print-label page for full form display

--- app/components/print-label-menu/PrintLabelPopover.tsx
+++ app/components/print-label-menu/PrintLabelPopover.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
 import QcLabelForm from './QcLabelForm';
 import GrnLabelForm from './GrnLabelForm';
+import { useRouter } from 'next/navigation';
 
 export default function PrintLabelPopover() {
+  const router = useRouter();
+
   return (
     <Tabs.Root defaultValue="qc" className="flex w-[340px] min-h-[420px]">
       <Tabs.List className="flex flex-col w-32 border-r border-gray-700 bg-[#23263a]">
-        <Tabs.Trigger value="qc" className="px-4 py-3 text-base font-semibold text-white text-left data-[state=active]:bg-gray-800 data-[state=active]:border-l-4 data-[state=active]:border-blue-500">QC Label</Tabs.Trigger>
-        <Tabs.Trigger value="grn" className="px-4 py-3 text-base font-semibold text-white text-left data-[state=active]:bg-gray-800 data-[state=active]:border-l-4 data-[state=active]:border-blue-500">GRN Label</Tabs.Trigger>
+        <Tabs.Trigger
+          value="qc"
+          className="px-4 py-3 text-base font-semibold text-white text-left data-[state=active]:bg-gray-800 data-[state=active]:border-l-4 data-[state=active]:border-blue-500"
+          onClick={() => router.push('/print-label')}
+        >
+          QC Label
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="grn"
+          className="px-4 py-3 text-base font-semibold text-white text-left data-[state=active]:bg-gray-800 data-[state=active]:border-l-4 data-[state=active]:border-blue-500"
+        >
+          GRN Label
+        </Tabs.Trigger>
       </Tabs.List>
       <div className="flex-1">
         <Tabs.Content value="qc">
