3e8cc65098fbeb63b27e62d5733a2518ee6f95e7
aiexkwan <crape.rambles.9j@icloud.com>
2025-05-22 14:50:18 +0100

    Refactor: Remove unused Supabase import in PalletRatio component

--- app/components/PalletRatio.tsx
+++ app/components/PalletRatio.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+// import { supabase } from '../../lib/supabase'; // This line will be removed
 import { DocumentIcon, ArrowsRightLeftIcon } from '@heroicons/react/24/outline';
 import PalletDonutChart from './PalletDonutChart';
 
